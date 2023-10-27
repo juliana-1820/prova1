4cb8b9700dc16e1505444593e776a47f813ed620
juliana-1820 <144728074+juliana-1820@users.noreply.github.com>
2023-10-27 20:27:23 -0300

    Add files via upload

---
+++ professor/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class professor(models.Model):
+    nome = models.CharField(max_length=100)
+    sexo= models.CharField(max_length=100)
+    registro = models.CharField(max_length=100)
+    datanascimento=models.DateField
